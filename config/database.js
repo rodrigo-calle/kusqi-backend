ab6005e2c2d5e5459efa2aeb165d2892b3e837f4
Rodrigo Calle Castillo <69275525+rodrigo-calle@users.noreply.github.com>
2022-07-20 23:58:24 -0500

    add comments for code deprecated

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ const URI = process.env.DB_URI;
 async function connectDB() {
   try {
     await mongoose.connect(URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+      useNewUrlParser: true, // is no longer supported https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options
+      useUnifiedTopology: true // is no longer supported
     });
     console.log('MongoDB connected!!');
   } catch (err) {
